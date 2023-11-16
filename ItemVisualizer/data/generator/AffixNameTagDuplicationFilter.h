17ad8f7d0c474ddb8a1b75fcc453719f8b6a5b07
Jan Randmets <rand.jan@gmail.com>
2023-11-16 02:37:58 +0200

    Duplicate algorithm better

--- ItemVisualizer/data/generator/AffixNameTagDuplicationFilter.h
+++ ItemVisualizer/data/generator/AffixNameTagDuplicationFilter.h
@@ -21,28 +21,12 @@ public:
 
     virtual bool acceptAffixRoll(const PossibleAffix& affixRoll) const override
     {
-        for(const auto& nameTag: m_NameTagCache)
-        {
-            //qDebug() << "Test " << acceptedAffix->nameTag.c_str() << " vs " << affixRoll.nameTag.c_str();
-            if(nameTag.compare(affixRoll.nameTag) == 0)
-            {
-
-                qDebug() << "Declined: " << affixRoll.nameTag.c_str();
-                return false;
-            }
-        }
-        qDebug() << "Accept: " << affixRoll.nameTag.c_str();
-
-        return true;
-    }
-    /*
         bool contains = std::any_of(m_NameTagCache.begin(), m_NameTagCache.end(), [affixRoll](const NameTag& nameTag){
-            return .compare(affixRoll.nameTag) == 0;
+            return affixRoll.nameTag.compare(nameTag) == 0;
         });
 
         return !contains;
     }
-    */
 };
 
 
